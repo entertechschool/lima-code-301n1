bb0535489c5da78f561ffb8fd692662f20cd0da9
bdiazc90 <bdiazc90@gmail.com>
2024-08-20 17:48:15 -0500

    class-03/demos udpate

---
+++ curriculum/class-03/demos/spread-operator/script.js
@@ -0,0 +1,13 @@
+/**
+ * spread operator:
+ * permite desempaquetar los elementos de un objeto <<iterable>>
+ * ej. Arrays, Object.propiedades
+ */
+
+const numbers = [1, 2, 3, 4, 5];
+
+const maxNumber = Math.max(numbers);
+const minNumber = Math.min(numbers);
+
+console.log(maxNumber);
+console.log(minNumber);
