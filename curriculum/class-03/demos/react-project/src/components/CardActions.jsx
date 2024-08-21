0c4ea103a58db1f30f65f962f18344d80f0589f3
bdiazc90 <bdiazc90@gmail.com>
2024-08-20 22:19:12 -0500

    adding Modal from Bootstrap as child of CardActions

--- curriculum/class-03/demos/react-project/src/components/CardActions.jsx
+++ curriculum/class-03/demos/react-project/src/components/CardActions.jsx
@@ -1,6 +1,10 @@
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
+import ModalActions from './ModalAction';
 
 function CardActions(props) {
+
+    const [openModal, setOpenModal] = useState(false);
 
     function aumentar() {
         props.aumentar();
@@ -12,12 +16,18 @@ function CardActions(props) {
         props.setContadorcito(props.contadorcito - 100);
     }
 
+    function handleModal() {
+        setOpenModal(true);
+    }
+
     return (
         <>
             <h3>soy CardActions.jsx (child)</h3>
             <p>Contadorcito: {props.contadorcito}</p>
             <Button variant="success" onClick={aumentar}>Aumentar padre +</Button>
-            <Button variant="info" onClick={disminuir}>Disminuir padre -</Button>
+            <Button variant="warning" onClick={disminuir}>Disminuir padre -</Button>
+            <Button variant="primary" onClick={handleModal}>Abrir Modal</Button>
+            <ModalActions openModal={openModal} setOpenModal={setOpenModal} />
         </>
     )
 }
