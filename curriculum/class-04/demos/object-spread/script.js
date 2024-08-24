7cdee199f7716000c05c01c65f52854ba48fe976
bdiazc90 <bdiazc90@gmail.com>
2024-08-23 20:55:14 -0500

    class-04/objects-spread

--- curriculum/class-04/demos/object-spread/script.js
+++ curriculum/class-04/demos/object-spread/script.js
@@ -1,23 +1,41 @@
 // Ejemplo 1:
-const obj = { name: "bunnies" };
-console.log("Obj is", obj);
+const obj = { name: "bruno" };
+console.log("Obj is: ", obj);
 
 const copy = obj;
-console.log("Copy is", copy);
+console.log("Copy is: ", copy);
 
-copy.name = "rabbit";
-console.log("Copy is", copy);
-console.log("Obj is also changed to", obj); // what???
+copy.name = "matias";
+console.log("Copy is: ", copy);
+console.log("Obj is: ", obj); // what???
 
 // utiliza la desestructuración para crear un nuevo clon desconectado del original
+const clone = { ...obj };
+console.log("Clone is: ", clone);
+
+clone.name = "georgina";
+console.log("Clone is: ", clone);
+console.log("Obj is: ", obj);
+return;
 
 // Ejemplo 2: También podemos asignar valores utilizando la deconstrucción
 // Actualiza el objeto con los elementos anteriores (... obj) y agrégarle una nueva propiedad
 // Nota: utilizamos let antes, ¿alguna idea de por qué?
-let objeto = { name: "John" };
-objeto = { ...objeto, age: 55 };
+let persona = { name: "angel", lastname: "paz", country: "peru" };
+persona = { ...persona, age: 30 };
+console.log("persona: ", persona);
 
 // También puedes hacer esto con clones y mantenerlos desconectados
+const personaHomonima = { ...persona, age: 50, age: 80 };
+// Si queremos modificar a personaHomonima, no podremos porque se declaro como "const"
+console.log("personaHomonima: ", personaHomonima);
 
 // Ejemplo 3: Los arrays funcionan de la misma manera. Agrega un elemento al mismo array:
-let family = ["Me", "You", "Kid"];
+const family = ["Me", "You", "Kid"];
+
+family.push("Baby");
+
+const familyExtended = [...family, "Dog"];
+
+console.log(family);
+console.log(familyExtended);
